b046eb54137c5aecf423de48891ed0ccaaefb649
Argorn <60606334+argorn-dev@users.noreply.github.com>
2021-07-12 16:09:05 -0700

    Revert "format currency on flash"
    This reverts commit 5ca88faea0571b0155e56672fd90801549787f56.

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def index():
         elif beneficiary == "General Family":
             beneficiary_sms(contributor_name, amount, contributor_contact, beneficiary, general_family)
         
-        flash("{:,.2f}GHS from {} processed for {}".format(amount,contributor_name,beneficiary))
+        flash("{} GHS from {} processed for {}".format(amount,contributor_name,beneficiary))
         return redirect(url_for("index"))
 
     return render_template("index.html",admin_name = admin_name)
