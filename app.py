74f7f918e0f99081112e2582a35a3859e1e90440
Argorn <60606334+argorn-dev@users.noreply.github.com>
2021-08-01 14:24:59 +0000

    Revert "fiifi's custom"
    This reverts commit 3d06afa5d33d706950269311a8237c0b0d5c5400.

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from methods import *
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8zffhgf46@@]'
 
-fiifi = "0243288066"
-general_family = "0546353625"
+gideon = "0208162005"
+ekow = "0550726756"
+augusta = "0244879849"
+general_family = "0208162005"
 
 TABLE_NAME = "contributor"
 
